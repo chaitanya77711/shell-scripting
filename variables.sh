265b37f12abd1eadaf3d660afdf92bd4216067a0
chaitu <chaitanyachaitu157@gmail.com>
2026-02-24 10:23:44 +0530

    update

--- variables.sh
+++ variables.sh
@@ -3,14 +3,14 @@
 Person1=mahi
 Person2=chaitanya
 
-echo "Person1:: Hi Person2, how do you stay calm in pressure, Person2?"
+echo "$Person1:: Hi $Person2, how do you stay calm in pressure, $Person2?"
 
-echo "Person2:: Person1, focus on the process. Pressure disappears, Person1."
+echo "$Person2:: $Person1, focus on the process. Pressure disappears, $Person1."
 
-echo "Person1:: Person2, what should I do after failure, Person2?"
+echo "$Person1:: $Person2, what should I do after failure, $Person2?"
 
-echo "Person2:: Learn fast and move forward, Person1."
+echo "$Person2:: Learn fast and move forward, $Person1."
 
-echo "Person1:: One life advice, Person2?"
+echo "$Person1:: One life advice, $Person2?"
 
-echo "Person2:: Stay simple and consistent, Person1."
+echo "$Person2:: Stay simple and consistent, $Person1."
