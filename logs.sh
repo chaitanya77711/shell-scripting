cbd3fd81773fc7fc2d17d5b13ac649507b22162f
chaitu <chaitanyachaitu157@gmail.com>
2026-03-02 17:37:41 +0530

    update

---
+++ logs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+userid=$(id -u)
+log_folder="/var/log/shell-script"
+log_file="/var/log/shell-script/$0.log"
+
+if [ $userid -ne 0 ]; then
+   echo "please run with root user"
+   exit 1
+fi 
+
+validate(){
+    if [ $1 -ne 0 ]; then
+    echo "$2 failure"
+    exit 1
+
+    else
+       echo " $2 success adi ra abbbai"
+    fi
+} 
+
+dnf install nginx -y &>> $log_file
+validate $? "installing nginx"
+
+dnf install mysql -y &>> $log_file
+validate $? "installing mysql"
+
