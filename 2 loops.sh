15b9560c3907e5a091dae10b5ded350bc07f6d38
chaitu <chaitanyachaitu157@gmail.com>
2026-03-02 19:41:35 +0530

    update

---
+++ 2 loops.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+userid=$(id -u)
+log_folder="/var/log/shell-script"
+log_file="/var/log/shell-script/$0.log"
+
+if [ $userid -ne 0 ]; then
+   echo "please run with root user" |tee -a $log_file
+   exit 1
+fi 
+
+validate(){
+    if [ $1 -ne 0 ]; then
+    echo "$2 failure" | tee -a $log_file
+    exit 1
+
+    else
+       echo " $2 success adi ra abbbai" | tee -a $log_file
+    fi
+} 
+
+for package in $@
+ do
+ dnf install $packages -y & >> $log_file
+ validate $? "$package installation"
+done
