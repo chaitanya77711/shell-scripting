c4b6641d86420ff4a8d9acd55fcef512537b4b0e
chaitu <chaitanyachaitu157@gmail.com>
2026-03-02 18:18:16 +0530

    update

--- logs.sh
+++ logs.sh
@@ -5,17 +5,17 @@ log_folder="/var/log/shell-script"
 log_file="/var/log/shell-script/$0.log"
 
 if [ $userid -ne 0 ]; then
-   echo "please run with root user"
+   echo "please run with root user" |tee -a $log_file
    exit 1
 fi 
 
 validate(){
     if [ $1 -ne 0 ]; then
-    echo "$2 failure"
+    echo "$2 failure" | tee -a $log_file
     exit 1
 
     else
-       echo " $2 success adi ra abbbai"
+       echo " $2 success adi ra abbbai" | tee -a $log_file
     fi
 } 
 
